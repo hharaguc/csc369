edebb4bb88ca750134822506bd44599aa3764b4d
Holly <hharaguc@calpoly.edu>
2017-02-17 00:41:35 -0800

    Test

--- lab6/MyMapReduceDriver.java
+++ lab6/MyMapReduceDriver.java
@@ -7,24 +7,23 @@
 // Section 1: Imports
 
 
-                  // Data containers for Map() and Reduce() functions
+// Data containers for Map() and Reduce() functions
 
-                  // You would import the data types needed for your keys and values
+// You would import the data types needed for your keys and values
 import org.apache.hadoop.io.IntWritable; // Hadoop's serialized int wrapper class
 import org.apache.hadoop.io.Text;        // Hadoop's serialized String wrapper class
 
 
-                 // For Map and Reduce jobs
+// For Map and Reduce jobs
 
 import org.apache.hadoop.mapreduce.Mapper; // Mapper class to be extended by our Map function
 import org.apache.hadoop.mapreduce.Reducer; // Reducer class to be extended by our Reduce function
 
-                 // To start the MapReduce process
+// To start the MapReduce process
 
 import org.apache.hadoop.mapreduce.Job; // the MapReduce job class that is used a the driver
 
-
-                // For File "I/O"
+// For File "I/O"
 
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; // class for "pointing" at input file(s)
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat; // class for "pointing" at output file
@@ -44,7 +43,7 @@ public class MyMapReduceDriver {
 
 
 public static class MyMapperClass     // Need to replace the four type labels there with actual Java class names
-     extends Mapper< InKeyType, InValueType, OutKeyType, OutValueType> {
+     extends Mapper< LongWritable, Text, LongWritable, Text> {
 
  @Override   // we are overriding Mapper's map() method
 
@@ -53,16 +52,28 @@ public static class MyMapperClass     // Need to replace the four type labels th
 // second parameter: input value
 // third parameter: container for emitting output key-value pairs
 
-public void map(InKeyType key, InValueType value, Context context)
+public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
 
    // put your map() code in here
-
+   String name = "";
+   String text[] = value.toString().split(",");
+   
    // analyze input 
+   if (text.length == 3) {
+      name = text[1];
+      String newKey = text[0];
+      int myKey = Long.parseLong(newKey);
+      
+      if (name.charAt(0) == 'H') {
+         name = "Holly Haraguchi";
+      }
+      Text out = new Text(name);
+      LongWritable outKey = new LongWritable(myKey);
+      context.write(outKey, out);
+   }
+   // emit output. Use the following for emitting output:
 
-  // emit output. Use the following for emitting output:
-
-    context.write(new OutKeyType(outKeyValue), new OutValueType(outValue));
 
 
  } // map
@@ -74,7 +85,7 @@ public void map(InKeyType key, InValueType value, Context context)
 // Section 3: Reducer Class Template
 
 public static class MyReducerClass   // needs to replace the four type labels with actual Java class names
-      extends  Reducer< InKeyType,  InValueType, OutKeyType, OutValueType> {
+      extends  Reducer< LongWritable,  Text, LongWritable, Text> {
 
  // note: InValueType is a type of a single value Reducer will work with
  //       the parameter to reduce() method will be Iterable<InValueType> - i.e. a list of these values
@@ -86,21 +97,21 @@ public static class MyReducerClass   // needs to replace the four type labels wi
 // second parameter: a list of values associated with the key
 // third parameter: container  for emitting output key-value pairs
 
-public void reduce( InKeyType key, Iterable<InValueType> values, Context context)
+public void reduce( LongWritable key, Iterable<Text> values, Context context)
      throws IOException, InterruptedException {
 
   // Your code goes here 
 
-
+  String name = "";
   // roll through all values in the provided Iterable
-  for (InValueType val : values) {
+  for (Text val : values) {
   
      // do the work
-
+     name = val.toString();
   } // for
 
   // emit final output
-  context.write(key, YourComputedValue);   
+  context.write(key, new Text(name));   
 
 
  } // reduce
@@ -123,22 +134,22 @@ public void reduce( InKeyType key, Iterable<InValueType> values, Context context
       job.setJarByClass(MyMapReduceDriver.class);  
 
    //  step 3:  Set Input and Output files
-       FileInputFormat.addInputPath(job, new Path(...)); // put what you need as input file
-       FileOutputFormat.addOutputPath(job, new Path(...)); // put what you need as output file
+       FileInputFormat.setInputPaths(job, new Path(args[0])); // put what you need as input file
+       FileOutputFormat.setOutputPath(job, new Path(args[1])); // put what you need as output file
 
    // step 4:  Register mapper and reducer
-      job.setMapperClass(MyMapper.class);
-      job.setReducerClass(MyReducer.class);
+      job.setMapperClass(MyMapperClass.class);
+      job.setReducerClass(MyReducerClass.class);
   
    //  step 5: Set up output information
-       job.setOutputKeyClass(...) // specify the output class (what reduce() emits) for key
-       job.setOutputValueClass(...) // specify the output class (what reduce() emits) for value
+       job.setOutputKeyClass(LongWritable.class); // specify the output class (what reduce() emits) for key
+       job.setOutputValueClass(Text.class); // specify the output class (what reduce() emits) for value
 
    // step 6: Set up other job parameters at will
       job.setJobName("My Test Job");
 
    // step 7:  ?
-
+   
    // step 8: profit
       System.exit(job.waitForCompletion(true) ? 0:1);
 
